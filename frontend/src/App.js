141f1f4d2d354ba211deea305b5028556d2d09aa
Fidel C <coronel08@aol.com>
2021-04-07 15:34:17 -0700

    Sample data seprated into own section, api working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Flashcard from './Flashcard'
 import axios from 'axios'
 import data from './sampleData'
@@ -11,28 +11,29 @@ http://localhost:8000/api/v2/quizzes/1/all_questions/ all questions by quiz
 */
 
 function App() {
-    // const [quizzes, setQuizzes] = useState(data)
     const [quizzes, setQuizzes] = useState([])
+
+    // SAMPLE DATA FOR TESTING 
+    // const [quizzes, setQuizzes] = useState(data)
     // useEffect(() => {
-    //     axios
-    //     .get('http://127.0.0.1:8000/api/v2/questions/')
-    //     // .get('http://localhost:8000/api/v2/quizzes/')
-    //     .then(res => {
-    //         setQuizzes(res.data.results.map((questionItem, index) => {
-    //             const answers = questionItem.answers.map(answer => {
-    //                 return <div className="options">{answer.text}</div>
-    //             })
-    //             return {
-    //                 id: `${index}-${Date.now()}`,
-    //                 quiz: questionItem.quiz_title,
-    //                 question: questionItem.prompt,
-    //                 answers: answers
-    //             }
-    //         }))
-    //         // https://stackoverflow.com/questions/61909924/rendering-json-child-list-from-object-list-in-reactjs
-    //         console.log(res.data)
-    //     })
+    //     setQuizzes(quizzes.map((questionItem, index) => {
+    //         const correctAnswer = questionItem.answers.filter(options => options.correct.toString() === 'true').map(options => {
+    //             return options.text
+    //         })
+    //         const allAnswers = questionItem.answers.map(options => {
+    //             return <p key={options.id}> {options.text} </p>
+    //         })
+    //         return {
+    //             id: `${index}-${Date.now()}`,
+    //             quiz: questionItem.quiz_title,
+    //             prompt: questionItem.prompt,
+    //             allAnswers,
+    //             correctAnswer,
+    //         }
+    //     }))
     // }, [])
+
+
 
     useEffect(() => {
         axios
@@ -57,22 +58,18 @@ function App() {
             console.log(res.data)
         })
     }, [])
-    console.log(quizzes)
 
 
 
     return (
         <div className="App">
-            <h1>Header</h1>
+            <h1>Heading</h1>
             <div className="quiz">
-                {/* {quizzes.map(quiz => {
-                    return < Flashcard quiz={quiz} key={quiz.id} />
-                })} */}
-                {quizzes.map( function (quiz,index) {
+                {quizzes.map(function (quiz, index) {
                     return (
                         <React.Fragment key={index}>
                             <h4>{quiz.id}. {quiz.prompt}</h4>
-                            < Flashcard quiz={quiz} key={quiz.id}/>
+                            < Flashcard quiz={quiz} key={quiz.id} />
                         </React.Fragment>
                     )
                 })}
--- frontend/src/Flashcard.js
+++ frontend/src/Flashcard.js
@@ -3,12 +3,6 @@ import React, { useState } from 'react'
 
 export default function Flashcard({ quiz }) {
     const [flip, setFlip] = useState(false)
-    // const correctAnswer = quiz.filter(options => options.correct.toString() === 'true').map(options => {
-    //     return `${options.id}. ${options.text}`
-    // })
-    // const allAnswers = quiz.map(options => {
-    //     return <p key={options.id}> {options.id}. {options.text} </p>
-    // })
 
     return (
         <div className={`card ${flip ? 'flip' : ''}`}
