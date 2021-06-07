530981c03958ff451d392f8fd0544f97b6d375a1
Fidel C <coronel08@aol.com>
2021-06-07 14:55:18 -0700

    update sample data

--- frontend/src/sampleData.js
+++ frontend/src/sampleData.js
@@ -106,5 +106,302 @@ module.exports = [
                 correct:false
             }
         ]
+    },
+    {
+        id:5,
+        quiz_title:'Electrical Quiz',
+        prompt:'Your employee sets a cabinet wrong. The cabinet falls from the wall and damages the clients counter top. What should you do?',
+        answers: [
+            {
+                id:1,
+                text:'Claim the damage on your general liability insurance policy',
+                correct:true
+            },
+            {
+                id:2,
+                text:'Require the employee to pay for the damage to the counter top',
+                correct:false
+            },
+            {
+                id:3,
+                text:'Tell the client to claim the damaged counter top on his or her homeowners insurance policy.',
+                correct:false
+            },
+            {
+                id:4,
+                text:'No action is required: The client must assume the risk of damage',
+                correct:false
+            }
+        ]
+    },
+    {
+        id:6,
+        quiz_title:'Electrical Quiz',
+        prompt:'As required by the Immigration reform and Control Act, all employees hired on or after November 7,1986 must complete Form ___ to verify their employment eligiblity status.',
+        answers: [
+            {
+                id:1,
+                text:'I-9',
+                correct:true
+            },
+            {
+                id:2,
+                text:'W-4',
+                correct:false
+            },
+            {
+                id:3,
+                text:'US-99',
+                correct:false
+            },
+            {
+                id:4,
+                text:'N-14',
+                correct:false
+            }
+        ]
+    },
+    {
+        id:7,
+        quiz_title:'Electrical Quiz',
+        prompt:'How often are you required to pay sales tax?',
+        answers: [
+            {
+                id:1,
+                text:'Monthly',
+                correct:false
+            },
+            {
+                id:2,
+                text:'Quarterly',
+                correct:false
+            },
+            {
+                id:3,
+                text:'Annually',
+                correct:false
+            },
+            {
+                id:4,
+                text:'It depends on the expected amount of taxable sales.',
+                correct:true
+            }
+        ]
+    },
+    {
+        id:8,
+        quiz_title:'Electrical Quiz',
+        prompt:'All of the following are causes for disciplinary action except:',
+        answers: [
+            {
+                id:1,
+                text:'Allowing your license to be used by an unlicensed contractor',
+                correct:false
+            },
+            {
+                id:2,
+                text:'Entering into a contract with another contractor who is not licensed',
+                correct:false
+            },
+            {
+                id:3,
+                text:'during bankruptcy, settling obligations incurred as a contractor for less than the full amount owed',
+                correct:true
+            },
+            {
+                id:4,
+                text:'willful or deliberate failure to pay money when due for materials or services',
+                correct:false
+            }
+        ]
+    },
+    {
+        id:9,
+        quiz_title:'Electrical Quiz',
+        prompt:'How long does a client have to cnacel a contract for repairs that are the result of an event for which a state of emergency was declared?',
+        answers: [
+            {
+                id:1,
+                text:'5 business days',
+                correct:false
+            },
+            {
+                id:2,
+                text:'5 calendar days',
+                correct:false
+            },
+            {
+                id:3,
+                text:'7 business days',
+                correct:true
+            },
+            {
+                id:4,
+                text:'7 calendar days',
+                correct:false
+            }
+        ]
+    },
+    {
+        id:10,
+        quiz_title:'Electrical Quiz',
+        prompt:'A prime contractor entered into a construction contract with a subcontractor. An employee of the subcontractor was injured but the subcontractor had not secured workers compensation coverage. Which of the following statements best describes the situation, except in cases of fraud?',
+        answers: [
+            {
+                id:1,
+                text:'The subcontractor is guilty, but the prime contractor is not responsible',
+                correct:false
+            },
+            {
+                id:2,
+                text:'The prime contractor is guilty, but the subcontractor is not responsible',
+                correct:false
+            },
+            {
+                id:3,
+                text:'Neither the prime contractor nor the subcontractor are responsible',
+                correct:false
+            },
+            {
+                id:4,
+                text:'The subcontractor is guilty, but the prime contractor must take responsibility for the injuries',
+                correct:true
+            }
+        ]
+    },
+    {
+        id:11,
+        quiz_title:'Electrical Quiz',
+        prompt:'A contractor usually will not be held liable for a supplier going on strike, but if your supplier did go on strike what should you do?',
+        answers: [
+            {
+                id:1,
+                text:'Contact the owner immediately',
+                correct:true
+            },
+            {
+                id:2,
+                text:'contact the architect immediately',
+                correct:false
+            },
+            {
+                id:3,
+                text:'Franchise Tax Board',
+                correct:false
+            },
+            {
+                id:4,
+                text:'Board of Equalization',
+                correct:false
+            }
+        ]
+    },
+    {
+        id:12,
+        quiz_title:'Electrical Quiz',
+        prompt:'When is the last day a customer can cancel a contract that was signed on Friday December 21st?',
+        answers: [
+            {
+                id:1,
+                text:'the following Friday',
+                correct:false
+            },
+            {
+                id:2,
+                text:'the following Tuesday',
+                correct:false
+            },
+            {
+                id:3,
+                text:'the following Wednesday',
+                correct:true
+            },
+            {
+                id:4,
+                text:'the following Thursday',
+                correct:false
+            }
+        ]
+    },
+    {
+        id:13,
+        quiz_title:'Electrical Quiz',
+        prompt:'what is the most likely reason to lose a bid?',
+        answers: [
+            {
+                id:1,
+                text:'10% added to profit',
+                correct:false
+            },
+            {
+                id:2,
+                text:'vague specifications',
+                correct:true
+            },
+            {
+                id:3,
+                text:'prices from subs were not exact',
+                correct:false
+            },
+            {
+                id:4,
+                text:'costs of materials were not obtained',
+                correct:false
+            }
+        ]
+    },
+    {
+        id:14,
+        quiz_title:'Electrical Quiz',
+        prompt:'Who is responsible for checking the site before the building inspector arrives?',
+        answers: [
+            {
+                id:1,
+                text:'contractor',
+                correct:true
+            },
+            {
+                id:2,
+                text:'owner',
+                correct:false
+            },
+            {
+                id:3,
+                text:'foreperson',
+                correct:false
+            },
+            {
+                id:4,
+                text:'architect',
+                correct:false
+            }
+        ]
+    },
+    {
+        id:15,
+        quiz_title:'Electrical Quiz',
+        prompt:'A mechanic lien is a written document that must be signed and verified by the claimant. Step 1: obtain the legal description of the property. Step 3: Find out if the person requesting the work is the owner. What is step2?',
+        answers: [
+            {
+                id:1,
+                text:'Determine the amount of the lien claim',
+                correct:false
+            },
+            {
+                id:2,
+                text:'Find out the name of the legal owner of the property as well as the percent of ownership',
+                correct:true
+            },
+            {
+                id:3,
+                text:'List the attorney representing the contractor',
+                correct:false
+            },
+            {
+                id:4,
+                text:'List the attorney representing the owner',
+                correct:false
+            }
+        ]
     }
 ]
