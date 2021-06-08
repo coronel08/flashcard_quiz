ecbe6b1bdfbb60a12e2ec136a9f57735b26e11e4
Fidel C <coronel08@aol.com>
2021-06-07 22:13:12 -0700

    add function to auto set height of card

--- frontend/src/Flashcard.js
+++ frontend/src/Flashcard.js
@@ -1,21 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 
 export default function Flashcard({ flashcard }) {
     const [flip, setFlip] = useState(false)
+    const [height, setHeight] = useState('initial')
+
+    const frontElement = useRef()
+    const backElement = useRef()
+
+    function setMaxHeight(){
+        const frontHeight = frontElement.current.getBoundingClientRect().height
+        const backHeight = backElement.current.getBoundingClientRect().height
+        setHeight(Math.max(frontHeight, backHeight, 100))
+    }
+
+    useEffect(setMaxHeight, [flashcard.allAnswers, flashcard.correctAnswer])
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight)
+        return () => window.removeEventListener('resize', setMaxHeight)
+    },[])
 
     return (
         <div>
             <div className={`card ${flip ? 'flip' : ''}`}
+                style = {{height: height }}
                 onClick={() => setFlip(!flip)}
             >
-                <div className="front">
+                <div className="front" ref={frontElement}>
                     {flashcard.prompt}
                     <div className="flashcard-options">
                         {flashcard.allAnswers}
                     </div>
                 </div>
-                <div className="back">
+                <div className="back" ref={backElement}>
                     {flip ? flashcard.correctAnswer : flashcard.allAnswers}
                 </div>
             </div>
